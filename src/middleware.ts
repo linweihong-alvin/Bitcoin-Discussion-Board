fd5f6fa8400b0398cf0ec78d0e14c0c3596d22b9
linweih-alvin <lin.weih@northeastern.edu>
2025-11-26 00:20:09 -0500

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+// user type url > middleware > app > layout.tsx (render homepate)
+
+export async function middleware(request: NextRequest) {
+	//   const pathname = request.nextUrl.pathname;
+
+	//   // Handle root path
+	//   if (pathname === "/") {
+	//     return NextResponse.redirect(new URL("/en", request.url));
+	//   }
+
+	//   const cookieStore = await cookies();
+	//   const accessToken = cookieStore.get("token")?.value;
+	//   if (!accessToken) {
+	//     return NextResponse.redirect(new URL(`/en/verify`, request.url));
+	//   }
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/"],
+};
