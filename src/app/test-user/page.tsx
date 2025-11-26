41cfb3dcd9ee1d943a33f5a637cf8ea347b48c49
linweih-alvin <lin.weih@northeastern.edu>
2025-11-26 15:28:28 -0500

    add create user page

---
+++ src/app/test-user/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout";
+import Content from "@/modules/user/content";
+
+const Post = () => {
+	return (
+		<Layout>
+			<Content />
+		</Layout>
+	);
+};
+
+export default Post;
