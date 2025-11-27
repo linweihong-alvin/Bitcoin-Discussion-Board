9c83136e01d330b9d2394c8f36d972c8bb9262ec
linweih-alvin <lin.weih@northeastern.edu>
2025-11-27 12:49:34 -0500

    collect mock data in one place

---
+++ src/mocks/posts.ts
@@ -0,0 +1,24 @@
+export const mockPosts: Post[] = [
+	{
+		id: "1",
+		title: "Post 1",
+		content: "Content 1",
+		author: { id: 1, username: "alvin" },
+		createdAt: 0,
+	},
+	{
+		id: "2",
+		title: "Post 2",
+		content: "Content 2",
+		author: { id: 1, username: "alvin" },
+		createdAt: 0,
+	},
+];
+
+export const mockPost: Post = {
+	id: "1",
+	title: "Post 1",
+	content: "Content 1",
+	author: { id: 1, username: "alvin" },
+	createdAt: 0,
+};
