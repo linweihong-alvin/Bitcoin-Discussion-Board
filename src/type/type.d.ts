620b6d9b308beeb49327aa227040b6b430ddfff7
linweih-alvin <lin.weih@northeastern.edu>
2025-11-27 12:39:16 -0500

    add interface ts type check

--- src/type/type.d.ts
+++ src/type/type.d.ts
@@ -1,6 +1,22 @@
 interface Post {
-  id: string;
-  title: string;
-  content: string;
-  createdAt: number;
+	id: string;
+	title: string;
+	content: string;
+	createdAt: number;
+	author?: {
+		id: number;
+		username: string;
+	};
+}
+
+interface User {
+	id: number;
+	username: string;
+	email: string;
+}
+
+interface ApiResponse<T> {
+	status: number;
+	message: string;
+	data: T;
 }
