8feaa6219ed907be2a588c1a8ce325768f1f80af
linweih-alvin <lin.weih@northeastern.edu>
2025-11-27 12:47:35 -0500

    add author on the post

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -5,16 +5,22 @@ interface PostProps {
 const Post = ({ post }: PostProps) => {
 	// Safe destructuring with defaults (no error when post is undefined)
 	// if post?.title is missing/undefined, title will default to "--"
-	const { title = "--", content = "-", createdAt = 0 } = post || {};
+	const { title = "--", content = "-", createdAt = 0, author } = post || {};
 	return (
 		// border-b: bottom border width 1 px
 		<div className="w-full border-b border-white/10 py-6">
 			<h3 className="text-sm text-white font-bold">{title}</h3>
 			<p className="text-sm text-white/50 mt-2">{content}</p>
-			<p className="text-sm text-white/50 mt-2">
-				{new Date(createdAt).toLocaleString()}
-				{/* 2025-07-25 */}
-			</p>
+			<div className="flex justify-between items-center mt-2">
+				<p className="text-sm text-white/50">
+					{new Date(createdAt).toLocaleString()}
+				</p>
+				{author && (
+					<p className="text-sm text-white/50">
+						by {author.username}
+					</p>
+				)}
+			</div>
 		</div>
 	);
 };
