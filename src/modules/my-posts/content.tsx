17ab621abdb3e2a68a515f411b4c2c1107443fd6
linweih-alvin <lin.weih@northeastern.edu>
2025-11-28 13:57:08 -0500

    add comment

--- src/modules/my-posts/content.tsx
+++ src/modules/my-posts/content.tsx
@@ -5,8 +5,8 @@ import { cookies } from "next/headers";
 
 const MyPostsContent = async () => {
 	// 注意：這裡簡化了驗證邏輯
-	// 實際項目中你需要從 session 或 cookie 獲取用戶信息
-	// 這裡我們假設你會在客戶端傳遞 userId
+	// 需要從 session 或 cookie 獲取用戶資訊
+	// 假設會在客戶端傳遞 userId
 	// 目前: get user info from localstorage (not secure, user can modify)
 	// todo: use authentication
 
